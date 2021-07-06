0874350244ecbc0cbba7113901799c769ec545da
Renske <renske@fabrique.nl>
2021-07-06 10:45:06 +0200

    can retrieve items from backend

--- metahub/starling_metahub/structures/interfaces.py
+++ metahub/starling_metahub/structures/interfaces.py
@@ -1,4 +1,4 @@
-from typing import NamedTuple, Iterable
+from typing import NamedTuple, Iterable, Sequence
 
 from starling.interfaces.atoms import AtomLinkRegular
 
@@ -7,7 +7,9 @@ class StructureMenuBarRegular(NamedTuple):
     menu_items: Iterable[AtomLinkRegular] = ()
     menu_button_title: str = 'Menu'
     navigation_aria_label: str = 'navigation'
+    main_navigation_aria_label: str = 'main navigation'
     logo_href: str = ''
+    languages: Sequence = []
 
 
 class StructureFooterBarSimple(NamedTuple):
