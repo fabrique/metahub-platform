7d57754ba3120fad7e432ee1f5d7c659fdafb86a
Joost Froon <joost@goedideemedia.nl>
2021-07-06 17:49:43 +0200

    start home animation

--- frontend/website/assets/scripts/modules/title-tween.js
+++ frontend/website/assets/scripts/modules/title-tween.js
@@ -8,6 +8,33 @@ export default function TitleTween (element, start = 1, end = 2) {
     window.GSAP.set(element, { force3D: true }) // We're gonna be needing the GPU real soon
     window.GSAP.fromTo(element, { scaleY: start }, { scaleY: end, scrollTrigger: { trigger: element.parentNode, scrub: 0.15 }, ease: 'linear' })
 
+    const sections = document.querySelectorAll('.js-animate-section')
+
+    sections.forEach(section => {
+      const wrapper = section.querySelector('.js-animate-section__inner')
+      console.log('wrapper', wrapper)
+      console.log('section', section)
+      window.GSAP.set(wrapper, { scaleY: 0, transformOrigin: '50% 100%' })
+
+      const tl = window.GSAP.timeline({
+        scrollTrigger: {
+          trigger: section,
+          scrub: true,
+          start: 'top bottom',
+          end: 'top top'
+        },
+        ease: 'none'
+      })
+
+      tl.fromTo(wrapper, {
+        scaleY: 0,
+        transformOrigin: '50% 0%'
+      }, {
+        scaleY: 1,
+        transformOrigin: '50% 0%'
+      })
+    })
+
     // window.GSAP.set(card, { force3D: true }) // We're gonna be needing the GPU real soon
     // window.GSAP.fromTo(card, { y: start }, { y: end, scrollTrigger: { trigger: cardWrapper, scrub: 0 }, ease: 'linear' })
     // window.ScrollTrigger.create({ trigger: card, start: '100px bottom', onEnter: () => card.classList.add('composition-card--inview') })
