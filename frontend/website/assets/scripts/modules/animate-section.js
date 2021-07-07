5d2e3fc95df745006a4e5b2abf977a7426a54088
Joost Froon <joost@goedideemedia.nl>
2021-07-07 17:07:52 +0200

    homepage effects

---
+++ frontend/website/assets/scripts/modules/animate-section.js
@@ -0,0 +1,57 @@
+export default function AnimateSection (element) {
+  if (!element) {
+    return
+  }
+
+  const initAnimateSection = () => {
+    const sections = document.querySelectorAll('.js-animate-section')
+
+    sections.forEach(section => {
+      const inner = section.querySelector('.js-animate-section__inner')
+      const outer = section.querySelector('.js-animate-section__outer')
+      window.GSAP.set(inner, { force3d: true })
+      window.GSAP.set(outer, { force3d: true })
+
+      const tl = window.GSAP.timeline({
+        scrollTrigger: {
+          trigger: section,
+          scrub: 0,
+          start: 'top bottom',
+          end: 'top top'
+        },
+        ease: 'none'
+      })
+
+      tl.fromTo(inner, {
+        scaleY: 0,
+        transformOrigin: '50% 0%'
+      }, {
+        scaleY: 1,
+        transformOrigin: '50% 0%',
+        ease: 'none'
+      })
+
+      const biem = window.GSAP.timeline({
+        scrollTrigger: {
+          trigger: section,
+          scrub: 0,
+          start: 'top top',
+          end: 'bottom top'
+        },
+        ease: 'none'
+      })
+
+      biem.fromTo(outer, {
+        scaleY: 1,
+        transformOrigin: '50% 100%'
+      }, {
+        scaleY: 0,
+        ease: 'none',
+        transformOrigin: '50% 100%'
+      })
+    })
+  }
+
+  window.addEventListener('scrolltrigger-loaded', () => initAnimateSection())
+  window.addEventListener('resize', () => window.requestAnimationFrame(() => initAnimateSection()))
+}
