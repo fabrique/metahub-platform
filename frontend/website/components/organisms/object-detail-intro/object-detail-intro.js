5bd67325f41089f3c5a7da2d6ecbca4ce640bbe2
Joost Froon <joost@goedideemedia.nl>
2021-07-14 15:47:23 +0200

    added object detail title effect

--- frontend/website/components/organisms/object-detail-intro/object-detail-intro.js
+++ frontend/website/components/organisms/object-detail-intro/object-detail-intro.js
@@ -1,15 +1,22 @@
 
-// import Component from '../../../assets/scripts/modules/component'
-// import TitleTween from '../../../assets/scripts/modules/title-tween'
+import Component from '../../../assets/scripts/modules/component'
+import TitleTween from '../../../assets/scripts/modules/title-tween'
 
-// class ObjectDetailIntroComponent extends Component {
-//   init () {
-//     const title = this.element.querySelector('.object-detail__intro__titles')
+class ObjectDetailComponent extends Component {
+  init () {
+    const title = this.element.querySelector('.js-object-detail__intro__title')
+    const wrapper = '.js-object-detail__intro'
 
-//     this.tween = new TitleTween(title, -1.8, 4)
-//   }
-// }
+    this.tween = new TitleTween({
+      element: title,
+      wrapper: wrapper,
+      start: 1,
+      end: 2,
+      startTrigger: 'top top'
+    })
+  }
+}
 
-// window.addEventListener('init-load', () => document.querySelectorAll('.object-detail__intro').forEach(element => {
-//   element.instance = element.instance || new ObjectDetailIntroComponent(element)
-// }))
+window.addEventListener('init-load', () => document.querySelectorAll('.js-object-detail__intro').forEach(element => {
+  element.instance = element.instance || new ObjectDetailComponent(element)
+}))
