3a0c44280001cf74c25610ddd9f901540ab470dc
Elco Klingen <disillusion84@gmail.com>
2021-08-13 13:35:47 +0200

    wcag menu trap

--- frontend/website/components/structures/menu/menu.js
+++ frontend/website/components/structures/menu/menu.js
@@ -1,24 +1,33 @@
 
+import { createFocusTrap } from 'focus-trap'
+
 import Component from '../../../assets/scripts/modules/component'
 
 export default class MenuBarComponent extends Component {
   init () {
     const width = 1180
-    this.menu = this.element
+
+    this.wrapper = this.element.querySelector('.menu-bar__wrapper')
+
     this.menuButton = this.element.querySelector('.js-menu-bar__menu-btn')
     this.links = this.element.querySelector('.js-menu-bar__links')
     this.menuBar = this.element.querySelector('.js-menu-bar')
+
+    this.firstMenuItem = this.links.querySelector('.link')
+
     // Navigation dialog
     this.menuIsOpen = false
     this.menuButton.addEventListener('click', () => this.toggleMobileMenu())
 
+    this.initAccessibility()
+
     // Menu breaks on larger resolutions, so force close it.
 
-    if (window.innerWidth < width) {
-      this.menu.ariaLabel = 'Navigation dialog'
-    }
-
     window.addEventListener('resize', () => {
+      if (window.innerWidth < width) {
+        this.element.ariaLabel = 'Navigation dialog'
+      }
+
       if (!this.menuIsOpen || this.isTogglingMenu || !this.oldWidth || window.innerWidth === this.oldWidth) {
         this.oldWidth = window.innerWidth
         return
@@ -48,6 +57,12 @@ export default class MenuBarComponent extends Component {
       this.menuButton.setAttribute('aria-expanded', 'true')
       this.links.removeAttribute('hidden')
 
+      if (this.focusTrap) {
+        this.setAriaHiddenOnOtherElements(true)
+        this.hideOverlayTabbableElements(false)
+        this.focusTrap.activate()
+      }
+
       document.body.style.overflow = 'hidden'
       document.body.style.height = '100%'
       document.body.style.position = 'fixed'
@@ -61,6 +76,10 @@ export default class MenuBarComponent extends Component {
       this.menuButton.setAttribute('aria-expanded', 'false')
       this.links.hidden = true
 
+      if (this.focusTrap) {
+        this.focusTrap.deactivate()
+      }
+
       document.body.style.overflow = 'visible'
       document.body.style.height = 'auto%'
       document.body.style.position = 'static'
@@ -70,6 +89,46 @@ export default class MenuBarComponent extends Component {
     }
 
     this.isTogglingMenu = false
+  }
+
+  initAccessibility () {
+    this.setAriaHiddenOnOtherElements(false)
+    this.hideOverlayTabbableElements(true)
+
+    this.focusTrap = createFocusTrap(this.wrapper, {
+      onActivate: () => {
+        this.firstMenuItem.focus()
+        this.firstMenuItem.blur()
+      },
+      onDeactivate: () => {
+        // this.menuIsOpen = true
+        this.toggleMobileMenu()
+        this.menuButton.focus()
+      },
+      escapeDeactivates: true,
+      clickOutsideDeactivates: true,
+      returnFocusOnDeactivate: false
+    })
+  }
+
+  setAriaHiddenOnOtherElements (hidden = true) {
+    const nonModalDialogVisibleElements = [...document.querySelectorAll('.content')]
+    const modalDialogVisibleElements = [...document.querySelectorAll('.menu-overlay')]
+
+    nonModalDialogVisibleElements.forEach(element => element.setAttribute('aria-hidden', hidden))
+    modalDialogVisibleElements.forEach(element => element.setAttribute('aria-hidden', !hidden))
+  }
+
+  hideOverlayTabbableElements (hidden = true) {
+    const modalDialogTabbableElements = [...document.querySelectorAll('.menu-overlay a[href], .menu-overlay button')]
+
+    modalDialogTabbableElements.forEach(element => element.classList.toggle('is-hidden', hidden))
+
+    if (hidden) {
+      modalDialogTabbableElements.forEach(element => element.setAttribute('tabindex', -1))
+    } else {
+      modalDialogTabbableElements.forEach(element => element.removeAttribute('tabindex'))
+    }
   }
 }
 
