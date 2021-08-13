2e74f37705a61ffea9764e2aca7603e81a5428f0
Elco Klingen <disillusion84@gmail.com>
2021-08-13 10:55:12 +0200

    cookiebar & tweaks

--- frontend/website/assets/scripts/modules/animate-section.js
+++ frontend/website/assets/scripts/modules/animate-section.js
@@ -1,3 +1,4 @@
+
 export default function AnimateSection (element) {
   if (!element) {
     return
@@ -5,50 +6,23 @@ export default function AnimateSection (element) {
 
   const initAnimateSection = () => {
     const sections = document.querySelectorAll('.js-animate-section')
-    console.log('TEST')
+
     sections.forEach(section => {
       const inner = section.querySelector('.js-animate-section__inner')
       const outer = section.querySelector('.js-animate-section__outer')
-      window.GSAP.set(inner, { force3d: true, scaleY: 0, transformOrigin: '50% 100%' })
-      window.GSAP.set(outer, { force3d: true, scaleY: 0, transformOrigin: '50% 100%' })
 
-      const comming = window.GSAP.timeline({
-        scrollTrigger: {
-          trigger: section,
-          scrub: 0,
-          start: 'top bottom',
-          end: 'top top'
-        },
-        ease: 'none'
-      })
+      if (!inner || !outer) {
+        return
+      }
 
-      comming.fromTo(inner, {
-        scaleY: 0,
-        transformOrigin: '50% 0%'
-      }, {
-        scaleY: 1,
-        transformOrigin: '50% 0%',
-        ease: 'none'
-      })
+      window.GSAP.set(inner, { scaleY: 0, transformOrigin: '50% 0%' })
+      window.GSAP.set(outer, { scaleY: 1, transformOrigin: '50% 100%' })
 
-      const going = window.GSAP.timeline({
-        scrollTrigger: {
-          trigger: section,
-          scrub: 0,
-          start: 'top top',
-          end: 'bottom top'
-        },
-        ease: 'none'
-      })
+      const coming = window.GSAP.timeline({ scrollTrigger: { trigger: section, scrub: 0, start: 'top bottom', end: 'top top' }, ease: 'none' })
+      coming.fromTo(inner, { scaleY: 0, transformOrigin: '50% 0%' }, { scaleY: 1, transformOrigin: '50% 0%', ease: 'none' })
 
-      going.fromTo(outer, {
-        scaleY: 1,
-        transformOrigin: '50% 100%'
-      }, {
-        scaleY: 0,
-        ease: 'none',
-        transformOrigin: '50% 100%'
-      })
+      const going = window.GSAP.timeline({ scrollTrigger: { trigger: section, scrub: 0, start: 'top top', end: 'bottom top' }, ease: 'none' })
+      going.fromTo(outer, { scaleY: 1, transformOrigin: '50% 100%' }, { scaleY: 0, ease: 'none', transformOrigin: '50% 100%' })
     })
   }
 
