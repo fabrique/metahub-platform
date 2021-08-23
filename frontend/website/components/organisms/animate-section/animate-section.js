b88037c7933eb2b8f2a8847e17fe40aa254e4ba7
Elco Klingen <disillusion84@gmail.com>
2021-08-23 13:59:05 +0200

    logos fixes

--- frontend/website/components/organisms/animate-section/animate-section.js
+++ frontend/website/components/organisms/animate-section/animate-section.js
@@ -1,12 +1,29 @@
 
-import AnimateSection from '../../../assets/scripts/modules/animate-section'
 import Component from '../../../assets/scripts/modules/component'
 
 class AnimateSectionComponent extends Component {
   init () {
-    const section = this.element.querySelector('.js-animate-section')
+    window.addEventListener('scrolltrigger-loaded', () => this.initAnimateSection())
+    window.addEventListener('resize', () => window.requestAnimationFrame(() => this.initAnimateSection()))
+  }
 
-    this.animateSection = new AnimateSection(section)
+  initAnimateSection () {
+    console.log('eh')
+    const inner = this.element.querySelector('.js-animate-section__inner')
+    const outer = this.element.querySelector('.js-animate-section__outer')
+
+    if (!inner || !outer) {
+      return
+    }
+
+    window.GSAP.set(inner, { scaleY: 0, transformOrigin: '50% 0%' })
+    window.GSAP.set(outer, { scaleY: 1, transformOrigin: '50% 100%' })
+
+    this.coming = window.GSAP.timeline({ scrollTrigger: { trigger: this.element, scrub: 0, start: 'top bottom', end: 'top top' }, ease: 'none' })
+    this.coming.fromTo(inner, { scaleY: 0, transformOrigin: '50% 0%' }, { scaleY: 1, transformOrigin: '50% 0%', ease: 'none' })
+
+    this.going = window.GSAP.timeline({ scrollTrigger: { trigger: this.element, scrub: 0, start: 'top top', end: 'bottom top' }, ease: 'none' })
+    this.going.fromTo(outer, { scaleY: 1, transformOrigin: '50% 100%' }, { scaleY: 0, ease: 'none', transformOrigin: '50% 100%' })
   }
 }
 
